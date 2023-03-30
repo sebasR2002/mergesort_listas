703e550d9576c426d6872a5d13cf3f5a5ae308a7
SEBASTIAN RESTREPO <sebas20022010@gmail.com>
2023-03-29 21:56:48 -0500

    agrego mi rama

--- src/Taller3.java
+++ src/Taller3.java
@@ -1,5 +1,19 @@
+
 public class Taller3 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+    }
+
+    public ListaSimple[] splitList(ListaSimple list) throws Exception {
+        ListaSimple[] split = new ListaSimple[2];
+        int div = (list.size() - 1) / 2;
+        for (int i = 0; i <= div; i++) {
+            split[0].addLast(list.get(i));
+        }
+        for (int i = 0; i > div && i < list.size(); i++) {
+            split[1].addHead(list.get(i));
+        }
+        return split;
     }
 }
