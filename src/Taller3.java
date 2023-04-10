72bd5e02d75a5e377e5ee01fb0f53567d88d74c7
SEBASTIAN RESTREPO <sebas20022010@gmail.com>
2023-04-10 12:57:10 -0500

    version entregada a jorge

--- src/Taller3.java
+++ src/Taller3.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Taller3 {
 
     public static void main(String[] args) throws Exception {
-        int N = 16000;
+        int N = 32000;
         List<Person> personas = new ArrayList<>();
         personas = GeneradorADTs.generar(N);
         ListaSimple<Comparable> personasimple = new ListaSimple<>();
@@ -39,6 +39,7 @@ public class Taller3 {
         aux = a.splitList();
         ListaSimple<Comparable> x = sort(aux[0]);
         ListaSimple<Comparable> y = sort(aux[1]);
+
         return a.fusionar(x, y);
 
     }
